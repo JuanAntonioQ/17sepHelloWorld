9f50c20ac464bc0a9930348b537bbdd2613d38b4
Juan Antonio <Juan Antonio@JuanAntonio-PC>
2015-09-17 10:15:17 +0100

    birth and age

--- src/pkg17sephelloworld/Person.java
+++ src/pkg17sephelloworld/Person.java
@@ -1,12 +1,21 @@
 package pkg17sephelloworld;
 
+import java.util.Date;
+
 public class Person {
+    public final Date dateOfBirth;
+    public int getAge(){
+        throw new RuntimeException("no implementado");
+    }
+            
     private final String firstName;
     private final  String familyName;
         
-    public Person(String firstName, String familyName) {
+    public Person(String firstName, String familyName, Date dateOfBirth) {
         this.firstName = firstName;
         this.familyName = familyName;
+        this.dateOfBirth = dateOfBirth;
+        
     }
 
     public String getFirstName() {
