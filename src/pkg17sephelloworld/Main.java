f1e282e65e8419cf914ce9e09d870d528549a8b1
Juan Antonio <Juan Antonio@JuanAntonio-PC>
2015-09-17 10:02:55 +0100

    full commit of first version

---
+++ src/pkg17sephelloworld/Main.java
@@ -0,0 +1,14 @@
+package pkg17sephelloworld;
+
+public class Main {
+    //public static final int MAX_FICHEROS_ABIERTOS = 99;
+    
+    public static void main(String[] args) {
+        Person yomismo;
+        yomismo = new Person("Perico", "de los Palotes");
+        //System.out.println("me llamo", yomismo.getFullName());
+    }
+    
+    
+    
+}
